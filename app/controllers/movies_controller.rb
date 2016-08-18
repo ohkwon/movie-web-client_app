318a6b0ed2faca618d91ec637473b0f6da2c6326
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-17 21:33:13 -0700

    add working new show and index actions

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,33 @@
+class MoviesController < ApplicationController
+
+  def show
+
+    @movie = Unirest.get("http://localhost:3000/api/v1/movies/#{params[:id]}.json").body
+
+  end
+
+  def index
+
+    @movies = Unirest.get("http://localhost:3000/api/v1/movies.json").body
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+    @movie = Unirest.post("http://localhost:3000/api/v1/movies.json",
+      headers: {Accept: "application/json"},
+      parameters: {
+        name: params[:name],
+        length: params[:length],
+        year: params[:year],
+        description: params[:description]
+      }
+    )
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/movies/new', to: 'movies#new'
+  post '/movies', to: 'movies#create'
+  get '/movies/:id', to: 'movies#show'
+  get '/movies', to: 'movies#index'
+  get '/movies/index', to: 'movies#index'
+
 end
