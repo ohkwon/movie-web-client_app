5625c3175b236d20b8fa8010ac5ba1d9b1a95f21
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-18 21:09:40 -0700

    add bucket in .env

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,13 +2,13 @@ class MoviesController < ApplicationController
 
   def show
 
-    @movie = Unirest.get("https://api-builder-movies-app.herokuapp.com/api/v1/movies/#{params[:id]}.json").body
+    @movie = Unirest.get("#{ENV['DOMAIN']}/movies/#{params[:id]}.json").body
 
   end
 
   def index
 
-    @movies = Unirest.get("https://api-builder-movies-app.herokuapp.com/api/v1/movies.json").body
+    @movies = Unirest.get("#{ENV['DOMAIN']}/movies.json").body
 
   end
 
@@ -18,7 +18,7 @@ class MoviesController < ApplicationController
 
   def create
 
-    @movie = Unirest.post("https://api-builder-movies-app.herokuapp.com/api/v1/movies.json",
+    @movie = Unirest.post("#{ENV['DOMAIN']}/movies.json",
       headers: {Accept: "application/json"},
       parameters: {
         name: params[:name],
@@ -34,13 +34,13 @@ class MoviesController < ApplicationController
 
   def edit
 
-    @movie = Unirest.get("https://api-builder-movies-app.herokuapp.com/api/v1/movies/#{params[:id]}.json").body
+    @movie = Unirest.get("#{ENV['DOMAIN']}/movies/#{params[:id]}.json").body
     
   end
 
   def update
 
-    @movie = Unirest.patch("https://api-builder-movies-app.herokuapp.com/api/v1/movies/#{params[:id]}.json",
+    @movie = Unirest.patch("#{ENV['DOMAIN']}/movies/#{params[:id]}.json",
       headers: {Accept: "application/json"},
       parameters: {
         name: params[:name],
