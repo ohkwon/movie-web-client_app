694c082331015eec16d87ddf6606785bf6661b09
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-18 18:51:21 -0700

    add edit, update, and delete actions to movies controller using apis

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,8 +28,40 @@ class MoviesController < ApplicationController
       }
     )
 
-    redirect_to '/movies'
+    redirect_to "/movies/#{@movie["id"]}"
 
+  end
+
+  def edit
+
+    @movie = Unirest.get("http://localhost:3000/api/v1/movies/#{params[:id]}.json").body
+    
+  end
+
+  def update
+
+    @movie = Unirest.patch("http://localhost:3000/api/v1/movies/#{params[:id]}.json",
+      headers: {Accept: "application/json"},
+      parameters: {
+        name: params[:name],
+        length: params[:length],
+        year: params[:year],
+        description: params[:description]
+      }
+    )
+
+    binding.pry
+
+    redirect_to "/movies/#{params["id"]}"
+    
+  end
+
+  def destroy
+
+    @movie = Unirest.delete("http://localhost:3000/api/v1/movies/#{params[:id]}.json").body
+
+    redirect_to "/movies"
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   
   get '/movies/new', to: 'movies#new'
   post '/movies', to: 'movies#create'
-  get '/movies/:id', to: 'movies#show'
   get '/movies', to: 'movies#index'
   get '/movies/index', to: 'movies#index'
+  get '/movies/:id/edit', to: 'movies#edit'
+  patch '/movies/:id', to: 'movies#update'
+  delete '/movies/:id', to: 'movies#destroy'
+  get '/movies/:id', to: 'movies#show'
 
 end
